b306973a4dba974ea14090599b2654b901f02e34
Øystein Thoen Sisjord <oystein@arkitektum.no>
2021-04-29 22:11:07 +0200

    Flytta solution og prosjekt eit nivå opp

---
+++ Dibk.Ftpb.Validation.Application/Models/ValidationEntities/Metadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dibk.Ftpb.Validation.Application.Models.ValidationEntities
+{
+    public class Metadata
+    {
+        public string FraSluttbrukersystem { get; set; }
+        public string Prosjektnavn { get; set; }
+    }
+}
