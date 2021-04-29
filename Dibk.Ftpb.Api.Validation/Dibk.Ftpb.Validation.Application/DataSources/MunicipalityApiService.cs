53f568b0aad6b1ebcd3507ab2ad873bf56c68480
Matias Gonzalez Tognoni <matias@arkitektum.no>
2021-04-29 19:50:43 +0200

    External Data Sources 📤&📥

--- Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Application/DataSources/MunicipalityApiService.cs
+++ Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Application/DataSources/MunicipalityApiService.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dibk.Ftpb.Validation.Application.DataSources.Models;
 
 namespace Dibk.Ftpb.Validation.Application.DataSources
 {
     public class MunicipalityApiService
     {
+        public MunicipalityViewModel GetMunicipality(string municipalityCode)
+        {
+            return new MunicipalityViewModel();
+        }
     }
 }
