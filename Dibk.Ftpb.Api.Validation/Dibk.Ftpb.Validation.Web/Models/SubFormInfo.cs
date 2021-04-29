99bc215d8c183fe332d29485abecdba20fa5975d
Øystein Thoen Sisjord <oystein@arkitektum.no>
2021-04-29 22:07:27 +0200

    Endra feilmelding

--- Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Web/Models/SubFormInfo.cs
+++ Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Web/Models/SubFormInfo.cs
@@ -11,7 +11,7 @@ namespace Dibk.Ftpb.Validation.Web.Models
         /// <summary>
         /// Name is attachmentType for attachment and form name for form and subforms
         /// </summary>
-        [Required(ErrorMessage = "Vedlegget må ha et navn")]
+        [Required(ErrorMessage = "Underskjema må ha ett navn")]
         public string FormName { get; set; }
 
         /// <summary>
