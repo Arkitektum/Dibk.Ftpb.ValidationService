9920cb01b75a82e215cfd71d261d0594f84d0e7f
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2021-04-29 21:14:02 +0200

    La til controller-metode

--- Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Web/Controllers/ValidateController.cs
+++ Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Web/Controllers/ValidateController.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Dibk.Ftpb.Validation.Application.Services;
 using Dibk.Ftpb.Validation.Web.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Dibk.Ftpb.Validation.Web.Controllers
 {
     [ApiController]
-    public class ValidateController : ControllerBase
+    public class ValidateController : BaseController
     {
+        private readonly IValidationService _validationService;
+
+        public ValidateController(
+            IValidationService validationService,
+            ILogger<ValidateController> logger) : base(logger)
+        {
+            _validationService = validationService;
+        }
 
         [Route("api/validate")]
         [HttpPost]
@@ -33,12 +40,34 @@ namespace Dibk.Ftpb.Validation.Web.Controllers
 
         }
 
+        [Route("api/validate/file")]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(object))]  // TODO!
+        public IActionResult ValidateFile(IFormFile file)
+        {
+            if (file == null)
+                return BadRequest();
+
+            try
+            {
+                var messages = _validationService.Validate(file);
+
+                return Ok(messages);
+            }
+            catch (Exception exception)
+            {
+                var result = HandleException(exception);
+
+                if (result != null)
+                    return result;
+
+                throw;
+            }
+        }
+
         private bool VerifyInput(ValidationInput input)
         {
             return !string.IsNullOrWhiteSpace(input.FormData);
         }
-
-
-
     }
 }
