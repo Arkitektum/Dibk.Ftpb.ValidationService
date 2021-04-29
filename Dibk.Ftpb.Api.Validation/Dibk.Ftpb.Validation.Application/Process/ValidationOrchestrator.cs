79c87fbac33836d5a6e31de96dc99d3b53f91caa
HelgeArkitektum Github <helge@arkitektum.no>
2021-04-29 20:11:30 +0200

    Empty models and validators

---
+++ Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Application/Process/ValidationOrchestrator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dibk.Ftpb.Validation.Application.Process
+{
+    /// <summary>
+    ///…InstantiateValidationMessageContainer(……)
+    ///…VerifyInputData(….)
+    ///….XSDValidate(….)
+    ///…DeserializeToMainForm(….)
+    ///…MapToFormEntity(….)
+    ///….ValidateFormEntity(….)
+    ///…CompileValidationReport(….)    /// </summary>
+    public class ValidationOrchestrator
+    {
+    }
+}
