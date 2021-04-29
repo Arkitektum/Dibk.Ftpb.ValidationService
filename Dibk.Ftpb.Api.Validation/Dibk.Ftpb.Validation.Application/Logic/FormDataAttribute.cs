783d381cb561927273c12391ab3b45d5c94fadee
HelgeArkitektum Github <helge@arkitektum.no>
2021-04-29 21:55:23 +0200

    Resolve formValidator

---
+++ Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Application/Logic/FormDataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dibk.Ftpb.Validation.Application.Logic
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FormDataAttribute : Attribute
+    {
+        //public string DataFormatId { get; set; }
+        public string DataFormatVersion { get; set; }
+
+        //public string ServiceCode { get; set; }
+    }
+}
--- Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Application/Logic/Metadata/AltinnConstraintsValidator.cs
+++ Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Application/Logic/Metadata/AltinnConstraintsValidator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dibk.Ftpb.Validation.Application.Logic.Metadata
 {
-    class AltinnConstraints
+    class AltinnConstraintsValidator
     {
     }
 }
