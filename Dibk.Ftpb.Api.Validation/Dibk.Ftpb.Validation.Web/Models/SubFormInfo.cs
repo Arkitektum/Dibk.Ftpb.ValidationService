7aed7e91ebd4bc14290889d312711bb38f4383c6
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2021-04-29 19:25:43 +0200

    Added API models

---
+++ Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Web/Models/SubFormInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dibk.Ftpb.Validation.Web.Models
+{
+    public class SubFormInfo
+    {
+        /// <summary>
+        /// Name is attachmentType for attachment and form name for form and subforms
+        /// </summary>
+        [Required(ErrorMessage = "Vedlegget må ha et navn")]
+        public string FormName { get; set; }
+
+        /// <summary>
+        /// filename with extension
+        /// </summary>
+        public string SubFormData { get; set; }
+
+    }
+}
---
+++ Dibk.Ftpb.Api.Validation/Dibk.Ftpb.Validation.Web/Models/ValidationInput.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dibk.Ftpb.Validation.Web.Models
+{
+
+
+    //{
+    //"FormData": "<?xml version=\"1.0\" encoding=\"utf-8\"?> <Nabovarsel  ... </xml>",
+    //"SubForms": [
+    //  {
+    //    "Name": "Situasjonsplan",
+    //  }, 
+    //  {
+    //    "Name": "TegningNyFasade",
+    //  }
+    // ],
+    //"Attachments": [
+    //  {
+    //    "Name": "Situasjonsplan",
+    //    "Filename": "minSitplan.pdf",
+    //    "FileSize": 30000
+    //  }, 
+    //  {
+    //    "Name": "TegningNyFasade",
+    //    "Filename": "tegning293456.pdf",
+    //    "FileSize": 500000
+    //  }
+    // ], 
+    //}
+
+
+
+    public class ValidationInput
+    {
+
+        /// <summary>
+        /// The Form data. https://www.altinn.no/api/help
+        /// </summary>
+        [Required]
+        public string FormData { get; set; }
+
+        /// <summary>
+        /// List of all subforms name used to validate required documentation. V2 enables more detailed validation of altinn rules for each attachement. Size, file extension and count of attachmenttypes.
+        /// </summary>
+        [Required]
+        public SubFormInfo[] SubForms { get; set; }
+
+        /// <summary>
+        /// List of all attachment types and forms/subforms name used to validate required documentation. V2 enables more detailed validation of altinn rules for each attachement. Size, file extension and count of attachmenttypes.
+        /// </summary>
+        [Required]
+        public AttachmentInfo[] Attachments { get; set; }
+
+
+
+
+    }
+}
