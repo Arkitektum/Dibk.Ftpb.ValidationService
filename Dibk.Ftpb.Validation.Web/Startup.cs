28878a24c39a24ab4434723a53bb2f8536845b8a
Jon Erlend Kronborg <jon.erlend@arkitektum.no>
2021-04-30 15:13:55 +0200

    Added Serilog
    Also added indexing to elk in dev. Set the url, usrname, pw to run

--- Dibk.Ftpb.Validation.Web/Startup.cs
+++ Dibk.Ftpb.Validation.Web/Startup.cs
@@ -7,6 +7,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+using System;
+using System.Configuration;
 
 namespace Dibk.Ftpb.Validation
 {
@@ -45,6 +50,8 @@ namespace Dibk.Ftpb.Validation
                      
             }
 
+            ConfigureLogging();
+
             app.UseSwagger();
             app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Dibk.Ftpb.Api.Validation v1"));
 
@@ -60,6 +67,37 @@ namespace Dibk.Ftpb.Validation
             {
                 endpoints.MapControllers();
             });
+        }
+        private void ConfigureLogging()
+        {
+            var logLevel = LogEventLevel.Debug;
+            var elasticSearchUrl = Configuration["Serilog:ConnectionUrl"];
+            var elasticUsername = Configuration["Serilog:Username"];
+            var elasticPassword = Configuration["Serilog:Password"];
+            var elasticIndexFormat = Configuration["Serilog:IndexFormat"];
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Is(logLevel)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .Enrich.WithWebApiControllerName()
+                .Enrich.WithWebApiActionName()
+                .Enrich.WithMvcControllerName()
+                .Enrich.WithMvcActionName()
+                .Enrich.WithMachineName()
+                /*.Enrich.WithBasicAuthUserName()*/
+                .Enrich.WithHttpRequestClientHostIP()
+                /*.Enrich.WithCorrelationIdHeader()*/
+                .WriteTo.Trace(outputTemplate: "{Timestamp:HH:mm:ss.fff} {SourceContext} [{Level}] {ArchiveReference} {Message}{NewLine}{Exception}")
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticSearchUrl))
+                {
+                    DetectElasticsearchVersion = true,
+                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                    AutoRegisterTemplate = true,
+                    ModifyConnectionSettings = x => x.BasicAuthentication(elasticUsername, elasticPassword),
+                    IndexFormat = elasticIndexFormat
+                })
+                .CreateLogger();
         }
     }
 }
