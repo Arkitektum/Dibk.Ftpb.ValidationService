d939d82e0546ae1064814e1eb84d88ef02eb4792
Matias Gonzalez Tognoni <matias@arkitektum.no>
2021-04-30 10:27:58 +0200

    Validation Entity

---
+++ Dibk.Ftpb.Validation.Application/Logic/EntityValidators/EiendomValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dibk.Ftpb.Validation.Application.Models.ValidationEntities;
+using Dibk.Ftpb.Validation.Application.Reporter;
+
+namespace Dibk.Ftpb.Validation.Application.Logic.EntityValidators
+{
+    public class EiendomValidator
+    {
+        public EiendomValidator()
+        {
+            
+        }
+        public List<ValidationMessage> Validate(string context, IEnumerable<Eiendom> eiendoms)
+        {
+            return new List<ValidationMessage>();
+        }
+
+        public List<ValidationMessage> EntityRules()
+        {
+            return new List<ValidationMessage>();
+        }
+    }
+}
