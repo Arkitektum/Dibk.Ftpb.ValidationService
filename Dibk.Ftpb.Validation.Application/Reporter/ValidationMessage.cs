46e6bba0412dbec66abc44da82c3eadc958a8e6b
Matias Gonzalez Tognoni <matias@arkitektum.no>
2021-04-30 10:27:26 +0200

    Add Validation messages Properties

--- Dibk.Ftpb.Validation.Application/Reporter/ValidationMessage.cs
+++ Dibk.Ftpb.Validation.Application/Reporter/ValidationMessage.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dibk.Ftpb.Validation.Application.Enums;
 
 namespace Dibk.Ftpb.Validation.Application.Reporter
 {
     public class ValidationMessage
     {
+
+        public string id;
+        public string xpath;
+        public ValidationMessageTypeEnum messagetype;
+
+
+        public string message;
+        public string preCondition;
+        public string checklistReference;
     }
 }
