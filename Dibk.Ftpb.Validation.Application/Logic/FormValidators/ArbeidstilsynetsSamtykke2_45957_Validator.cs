6006626df46eccf6b742597ff22494885afadbdc
HelgeArkitektum Github <helge@arkitektum.no>
2021-05-27 16:03:06 +0200

    Added abstract base class for forms

--- Dibk.Ftpb.Validation.Application/Logic/FormValidators/ArbeidstilsynetsSamtykke2_45957_Validator.cs
+++ Dibk.Ftpb.Validation.Application/Logic/FormValidators/ArbeidstilsynetsSamtykke2_45957_Validator.cs
@@ -15,14 +15,13 @@ using System.Linq;
 namespace Dibk.Ftpb.Validation.Application.Logic.FormValidators
 {
     [FormData(DataFormatVersion = "45957")]
-    public class ArbeidstilsynetsSamtykke2_45957_Validator : IFormValidator
+    public class ArbeidstilsynetsSamtykke2_45957_Validator : FormValidatorBase, IFormValidator
     {
         private readonly IMunicipalityValidator _municipalityValidator;
         private readonly ICodeListService _codeListService;
         public ArbeidstilsynetsSamtykke2Form_45957_ValidationEntity ArbeidstilsynetsSamtykke2Form45957 { get; set; }
         public ArbeidstilsynetsSamtykkeType _form { get; set; }
 
-        private ValidationResult _validationResult;
 
         //private readonly string _xPath = "ArbeidstilsynetsSamtykke";
         public ArbeidstilsynetsSamtykke2_45957_Validator(IMunicipalityValidator municipalityValidator, ICodeListService codeListService)
@@ -40,7 +39,7 @@ namespace Dibk.Ftpb.Validation.Application.Logic.FormValidators
             ArbeidstilsynetsSamtykke2Form45957 = new ArbeidstilsynetsSamtykkeV2Dfv45957_Mapper().GetFormEntity(_form);
             Validate(ArbeidstilsynetsSamtykke2Form45957, validationInput);
 
-            return _validationResult;
+            return ValidationResult;
         }
         public ArbeidstilsynetsSamtykkeType DeserializeDataForm(string xmlData)
         {
@@ -50,12 +49,6 @@ namespace Dibk.Ftpb.Validation.Application.Logic.FormValidators
 
         public ValidationResult Validate(ArbeidstilsynetsSamtykke2Form_45957_ValidationEntity form, ValidationInput validationInput)
         {
-
-
-            //Runner runner = new Runner();
-            //runner.Run();
-            //runner.FindNew();
-
             var eiendomValidator = new EiendomValidator(_municipalityValidator);
 
             foreach (var eiendomValidationEntity in form.ModelData.EiendomValidationEntities)
@@ -77,50 +70,7 @@ namespace Dibk.Ftpb.Validation.Application.Logic.FormValidators
             var fakturamottakerResult = new FakturamottakerValidator().Validate(form.ModelData.FakturamottakerValidationEntity);
             UpdateValidationResult(fakturamottakerResult);
 
-            return _validationResult;
-        }
-
-        internal void UpdateValidationResult(ValidationResult validationResult)
-        {
-            _validationResult ??= new ValidationResult();
-            _validationResult.ValidationRules ??= new List<ValidationRule>();
-            _validationResult.ValidationMessages ??= new List<ValidationMessage>();
-
-            //var whereNotAlreadyExists = validationResult.ValidationRules.Where(x => _validationResult.ValidationRules.All(y => y.Xpath != x.Xpath));
-            var whereNotAlreadyExists = validationResult.ValidationRules.Where(x => !_validationResult.ValidationRules.Any(y => y.Xpath == x.Xpath && y.Id == x.Id));
-            _validationResult.ValidationRules.AddRange(whereNotAlreadyExists);
-            _validationResult.ValidationMessages.AddRange(validationResult.ValidationMessages);
+            return ValidationResult;
         }
     }
-
-    public class Person
-    {
-        public int ID { get; set; }
-        public string LastName { get; set; }
-    }
-    public class Runner
-    {
-        List<Person> peopleList1 = new List<Person>();
-        List<Person> peopleList2 = new List<Person>();
-        public void Run()
-        {
-            peopleList1.Add(new Person() { ID = 1, LastName = "A" });
-            peopleList1.Add(new Person() { ID = 2, LastName = "B" });
-            peopleList1.Add(new Person() { ID = 3, LastName = "C" });
-                                                 
-            peopleList2.Add(new Person() { ID = 1, LastName = "A" });
-            peopleList2.Add(new Person() { ID = 2, LastName = "B" });
-            peopleList2.Add(new Person() { ID = 3, LastName = "W" });
-            peopleList2.Add(new Person() { ID = 4, LastName = "D" });
-            peopleList2.Add(new Person() { ID = 5, LastName = "E" });
-        }
-
-        public void FindNew()
-        {
-            //var result = peopleList2.Where(p => peopleList1.All(p2 => p2.ID != p.ID && p2.LastName != p.LastName));
-            var result = peopleList2.Where(p => !peopleList1.Any(p2 => p2.ID == p.ID && p2.LastName == p.LastName));
-        }
-    }
-
-
 }
