3b9b1261dd90a97f4cfad57f143122afa03c6db8
Matias <matias@arkitektum.no>
2021-06-16 09:57:22 +0200

    Update Mock for codelist

--- TestProject1/Utils/MockDataSource.cs
+++ TestProject1/Utils/MockDataSource.cs
@@ -37,10 +37,10 @@ namespace Dibk.Ftpb.Validation.Application.Tests.Utils
             codelistClient.Setup(a => a.IsCodelistValid(It.IsAny<ArbeidstilsynetCodeListNames>(), It.IsAny<string>())).Returns(valid);
             return codelistClient.Object;
         }
-        public static ICodeListService IsCodeListValid(FtbCodeListNames CodeListName, bool valid = true)
+        public static ICodeListService IsCodeListValid(FtbCodeListNames codeListName, bool valid = true)
         {
             var codelistClient = new Mock<ICodeListService>();
-            codelistClient.Setup(a => a.IsCodelistValid(It.IsAny<ArbeidstilsynetCodeListNames>(), It.IsAny<string>())).Returns(valid);
+            codelistClient.Setup(a => a.IsCodelistValid(It.IsAny<FtbCodeListNames>(), It.IsAny<string>())).Returns(valid);
             return codelistClient.Object;
         }
 
