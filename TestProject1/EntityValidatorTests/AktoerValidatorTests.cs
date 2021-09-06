321e94e18b92e748f255a03335ea44d1edecc62a
Matias <matias@arkitektum.no>
2021-09-06 15:16:49 +0200

    Update unit tests

---
+++ TestProject1/EntityValidatorTests/AktoerValidatorTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Dibk.Ftpb.Validation.Application.DataSources.ApiServices.CodeList;
+using Dibk.Ftpb.Validation.Application.Enums;
+using Dibk.Ftpb.Validation.Application.Logic.EntityValidators;
+using Dibk.Ftpb.Validation.Application.Logic.Interfaces;
+using Dibk.Ftpb.Validation.Application.Models.ValidationEntities;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.IO;
+using Dibk.Ftpb.Validation.Application.DataSources.ApiServices.CodeList;
+using Dibk.Ftpb.Validation.Application.DataSources.ApiServices.PostalCode;
+using Dibk.Ftpb.Validation.Application.Logic.EntityValidators.Common;
+using Dibk.Ftpb.Validation.Application.Tests.Utils;
+using Dibk.Ftpb.Validation.Application.Utils;
+using no.kxml.skjema.dibk.arbeidstilsynetsSamtykke2;
+using Xunit;
+
+namespace Dibk.Ftpb.Validation.Application.Tests.EntityValidatorTests
+{
+    public class AktoerValidatorTests
+    {
+        private ArbeidstilsynetsSamtykkeType _form;
+
+        private AktoerValidationEntity _aktoerValidationEntity;
+
+        private ICodeListService _codeListService;
+        private readonly IPostalCodeService _postalCodeService;
+
+        private IAktoerValidator _aktoerValidator;
+
+        public AktoerValidatorTests()
+        {
+            _postalCodeService = MockDataSource.ValidatePostnr(true, "Bø i telemark", "true");
+            _codeListService = MockDataSource.IsCodeListValid(FtbKodeListeEnum.Partstype, false);
+
+            var xmlData = File.ReadAllText(@"Data\ArbeidstilsynetsSamtykke_v2_dfv45957_Test.xml");
+            _form = SerializeUtil.DeserializeFromString<ArbeidstilsynetsSamtykkeType>(xmlData);
+            _aktoerValidationEntity = new Logic.Mappers.ArbeidstilsynetsSamtykke2.AktoerMapper(AktoerEnum.tiltakshaver).Map(_form.tiltakshaver, "UnitTest");
+
+            var tiltakshaverNodeList = new List<EntityValidatorNode>()
+            {
+                new () {NodeId = 1, EnumId = EntityValidatorEnum.TiltakshaverValidator, ParentID = null},
+                new () {NodeId = 2, EnumId = EntityValidatorEnum.KontaktpersonValidator, ParentID = 1},
+                new () {NodeId = 3, EnumId = EntityValidatorEnum.PartstypeValidator, ParentID = 1},
+                new () {NodeId = 4, EnumId = EntityValidatorEnum.EnkelAdresseValidator, ParentID = 1}
+            };
+            var tree = EntityValidatiorTree.BuildTree(tiltakshaverNodeList);
+
+            //Tiltakshaver
+            var tiltakshaverKontaktpersonValidator = new KontaktpersonValidator(tree, 2);
+            var tiltakshaverPartstypeValidator = new PartstypeValidator(tree, 3, _codeListService);
+            var tiltakshaverEnkelAdresseValidator = new EnkelAdresseValidator(tree, 4, _postalCodeService);
+            _aktoerValidator = new TiltakshaverValidator(tree, tiltakshaverEnkelAdresseValidator, tiltakshaverKontaktpersonValidator, tiltakshaverPartstypeValidator, _codeListService);
+
+
+        }
+        [Fact]
+        public void TestTilashaver()
+        {
+            //_aktoerValidationEntity.ModelData.Partstype.ModelData = null;
+
+            var tiltakshaverResult = _aktoerValidator.Validate(_aktoerValidationEntity);
+
+            tiltakshaverResult.Should().NotBeNull();
+        }
+    }
+}
--- TestProject1/EntityValidatorTests/EiendomByggestedValidatorTest.cs
+++ TestProject1/EntityValidatorTests/EiendomByggestedValidatorTest.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -13,6 +13,8 @@ using Dibk.Ftpb.Validation.Application.Models.ValidationEntities;
 using Dibk.Ftpb.Validation.Application.Tests.Utils;
 using Dibk.Ftpb.Validation.Application.Utils;
 using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using no.kxml.skjema.dibk.arbeidstilsynetsSamtykke2;
 using Xunit;
 
@@ -33,7 +35,9 @@ namespace Dibk.Ftpb.Validation.Application.Tests.EntityValidatorTests
 
         public EiendomByggestedValidatorTest()
         {
-            var xmlData = File.ReadAllText(@"Data\ArbeidstilsynetsSamtykke_v2_dfv45957.xml");
+            // Test data shoud be 
+            var xmlData = File.ReadAllText(@"Data\ArbeidstilsynetsSamtykke_v2_dfv45957_Test.xml");
+            //var xmlData = File.ReadAllText(@"C:\Temp\FTB - TestXML\ArbeidstilsynetsSamtykke_v2_empty.xml");
             _form = SerializeUtil.DeserializeFromString<ArbeidstilsynetsSamtykkeType>(xmlData);
 
             _eiendomValidationEntities = new Logic.Mappers.ArbeidstilsynetsSamtykke2.EiendomByggestedMapper().Map(_form.eiendomByggested, "ArbeidstilsynetsSamtykke");
@@ -47,12 +51,23 @@ namespace Dibk.Ftpb.Validation.Application.Tests.EntityValidatorTests
         }
 
 
-        [Fact(Skip = "noko")]
+        [Fact]
         public void EiendomTest()
         {
-            _eiendomValidationEntities.FirstOrDefault().ModelData.Adresse = null;
+            
+            //_eiendomValidationEntities.FirstOrDefault().ModelData.Matrikkel.ModelData.Kommunenummer = "";
+            //_eiendomValidationEntities.FirstOrDefault().ModelData.Matrikkel.ModelData.Gaardsnummer = "";
+            //_eiendomValidationEntities.FirstOrDefault().ModelData.Matrikkel.ModelData.Bruksnummer = "";
+            //_eiendomValidationEntities.FirstOrDefault().ModelData.Matrikkel.ModelData.Festenummer = "";
+            //_eiendomValidationEntities.FirstOrDefault().ModelData.Matrikkel.ModelData.Seksjonsnummer = "";
+
 
             var result = _eiendomByggestedValidator.Validate(_eiendomValidationEntities);
+            var ValidationMessages = result.ValidationMessages
+                .Select(r => string.Concat("Rule: ", r.Rule, "- xpath: ", r.XpathField)).ToArray();
+
+            var messageErrors = JArray.Parse(JsonConvert.SerializeObject(ValidationMessages));
+
             result.Should().NotBeNull();
         }
         [Fact(Skip = "noko")]
